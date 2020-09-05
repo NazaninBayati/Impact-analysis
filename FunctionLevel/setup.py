dd12e7b28dfd183dec5e93ed6f72ee6eb9993a14
NaziB <54472723+NazaninBayati@users.noreply.github.com>
2020-09-05 17:46:04 +0430

    update on function level

---
+++ FunctionLevel/setup.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('python pydriller.py /home/nazanin/ceph v15.2.0 v15.2.1')
+os.system('python function_scenario.py /path/to/current_version /path/to/previous_version')
+os.system('python arm.py')
